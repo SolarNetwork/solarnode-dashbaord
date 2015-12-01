b8fbc52f3a1030fb82f3493b53c1a0905245f115
Matt Magoffin <matt@solarnetwork.net>
2015-12-02 07:40:56 +1300

    Make the view a <fieldset> and add margin if not first.

--- app/components/configurable-chart-source-group.js
+++ app/components/configurable-chart-source-group.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'fieldset',
+  classNameBindings: ['notFirst:uk-margin-top'],
+  groupIndex: 0,
+  notFirst: Ember.computed('groupIndex', function() {
+    return (this.get('groupIndex') > 0);
+  }),
   sourceGroup: null,
   isFixedGroupCount: true,
   fixedGroupCount: 2
