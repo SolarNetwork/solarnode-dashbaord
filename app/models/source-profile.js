eb2a86c128722056ad8d68eff56b88af092fba5c
Matt Magoffin <matt@solarnetwork.net>
2015-12-09 13:19:48 +1300

    Remove unused model class.

--- app/models/source-profile.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  profile: DS.belongsTo('user-profile', {inverse:'sources'}),
-  source: DS.attr(),
-  props: DS.attr() // array of string
-});
