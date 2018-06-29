c63e6582e639785b58c39d4a52c119d5eb53fc53
Matt Magoffin <matt@solarnetwork.net>
2018-06-29 12:16:06 +1200

    JS-9: support a nodeId query parameter for the login form.

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,33 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend(UnauthenticatedRouteMixin);
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+
+    queryParams: {
+        nodeId: {
+            refreshModel: true
+        }
+    },
+  
+    beforeModel(params) {
+        if ( params && params.queryParams && params.queryParams.nodeId ) {
+            // always allow going to this route if a nodeId param provided
+        } else {
+            // otherwise let UnauthenticatedRouteMixin handle normally
+            this._super(...arguments);
+        }
+    },
+
+    model(params) {
+        var m = this._super(...arguments);
+        if ( m === undefined ) {
+            m = {};
+        }
+        if ( params && params.nodeId ) {
+            m.nodeId = params.nodeId;
+        }
+        return m;
+    }
+
+  });
+  
